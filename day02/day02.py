03763aaa46a61654f354251363783f730b94734d
Borja González-Seoane <garaje@glezseoane.es>
2021-12-02 16:06:49 +0100

    Solve day 2 part 2 puzzle

--- day02/day02.py
+++ day02/day02.py
@@ -47,19 +47,62 @@ and a depth of 10. (Multiplying these together produces 150.)
 Calculate the horizontal position and depth you would have after following
 the planned course. What do you get if you multiply your final horizontal
 position by your final depth?
+
+
+Part 2
+------
+
+Based on your calculations, the planned course doesn't seem to make any sense.
+You find the submarine manual and discover that the process is actually slightly
+more complicated.
+
+In addition to horizontal position and depth, you'll also need to track a third
+value, aim, which also starts at 0. The commands also mean something entirely
+different than you first thought:
+
+- `down X` increases your aim by X units.
+- `up X` decreases your aim by X units.
+- `forward X` does two things:
+    - It increases your horizontal position by X units.
+    - It increases your depth by your aim multiplied by X.
+
+Again note that since you're on a submarine, down and up do the opposite of
+what you might expect: "down" means aiming in the positive direction.
+
+Now, the above example does something different:
+
+- `forward 5` adds 5 to your horizontal position, a total of 5. Because your aim
+is 0, your depth does not change.
+- `down 5` adds 5 to your aim, resulting in a value of 5.
+- `forward` 8 adds 8 to your horizontal position, a total of 13. Because your
+aim is 5, your depth increases by 8*5=40.
+- `up 3` decreases your aim by 3, resulting in a value of 2.
+- `down 8` adds 8 to your aim, resulting in a value of 10.
+- `forward 2` adds 2 to your horizontal position, a total of 15. Because your
+aim is 10, your depth increases by 2*10=20 to a total of 60.
+
+After following these new instructions, you would have a horizontal position of
+15 and a depth of 60. (Multiplying these produces 900.)
+
+Using this new interpretation of the commands, calculate the horizontal
+position and depth you would have after following the planned course. What do
+you get if you multiply your final horizontal position by your final depth?
 """
 
 import doctest
 import time
 
 
-def calculate_final_position(movements: [str]) -> int:
+def calculate_final_position(movements: [str], consider_aim: bool = False) -> int:
     """Calculate the final position given a list of movements.
 
     Parameters
     ----------
     movements : [str]
         List of movements to perform.
+    consider_aim : bool
+        Consider the aim parameter in the final possition calculated. Default is
+        false to not consider it.
 
     Returns
     -------
@@ -70,17 +113,32 @@ def calculate_final_position(movements: [str]) -> int:
     --------
     >>> calculate_final_position(["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2", ])
     150
+    >>> calculate_final_position(["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2", ], consider_aim=True)
+    900
     """
     horizontal_position = depth = 0
-    for movement in movements:
-        command, units = movement.split(" ")
-        units = int(units)
-        if command == "forward":
-            horizontal_position += units
-        elif command == "down":
-            depth += units
-        else:
-            depth -= units
+    if not consider_aim:
+        for movement in movements:
+            command, units = movement.split(" ")
+            units = int(units)
+            if command == "forward":
+                horizontal_position += units
+            elif command == "down":
+                depth += units
+            else:
+                depth -= units
+    else:
+        aim = 0
+        for movement in movements:
+            command, units = movement.split(" ")
+            units = int(units)
+            if command == "forward":
+                horizontal_position += units
+                depth += aim * units
+            elif command == "down":
+                aim += units
+            else:
+                aim -= units
 
     return horizontal_position * depth
 
@@ -93,6 +151,11 @@ if __name__ == "__main__":
 
     time0 = time.time()
     final_position = calculate_final_position(movements)
-    timef = time.time()
+    time1 = time.time()
 
-    print(f"Final position: {final_position}. Time: {timef-time0:.8f}")
+    time2 = time.time()
+    final_position2 = calculate_final_position(movements, consider_aim=True)
+    time3 = time.time()
+
+    print(f"Part 1 final position: {final_position}. Time: {time1-time0:.8f}")
+    print(f"Part 2 final position: {final_position2}. Time: {time3-time2:.8f}")
